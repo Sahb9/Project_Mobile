7c37eb7c1ca1a64c5cc9dc23ef79483a6e9d5d5b
Sahb9 <71537975+Sahb9@users.noreply.github.com>
2022-04-05 16:14:42 +0700

    calendar test

---
+++ app/src/main/java/com/android/myapplication/DatePicker.java
@@ -0,0 +1,52 @@
+package com.android.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class DatePicker extends AppCompatActivity {
+
+    EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_date_picker);
+        editText = findViewById(R.id.editTextDate);
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ChonNgay();
+            }
+        });
+    }
+    private void ChonNgay()
+    {
+        // xóa khi bấm tiếp
+        int length = editText.getText().length();
+        if (length > 0) {
+            //Xóa khi bấm tiếp
+            editText.getText().delete(0, length);
+        }
+        //Select real-time
+        Calendar calendar = Calendar.getInstance();
+
+        //
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(android.widget.DatePicker datePicker, int i, int i1, int i2) {
+                calendar.set(i,i1,i2);
+                SimpleDateFormat dinhDangNgay = new SimpleDateFormat("dd/MM/yyyy");
+                editText.append(dinhDangNgay.format(calendar.getTime()));
+
+            }
+        },2021,calendar.get(Calendar.MONTH+1),calendar.get(Calendar.DATE));
+        datePickerDialog.show();
+    }
+}
--- app/src/main/java/com/android/myapplication/MainActivity.java
+++ app/src/main/java/com/android/myapplication/MainActivity.java
@@ -3,12 +3,35 @@ package com.android.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    TextView txtTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtTime = findViewById(R.id.textViewTime);
+        Calendar calendar = Calendar.getInstance();
+        txtTime.setText(calendar.getTime() + "\n");
+        txtTime.append(calendar.get(Calendar.DATE)+"\n");
+        txtTime.append(calendar.get(Calendar.MONTH)+"\n");// tháng đúng phải +1
+        txtTime.append(calendar.get(Calendar.YEAR)+"\n");//
+
+        //format ngày tháng năm
+        SimpleDateFormat dinhDangNgay = new SimpleDateFormat("dd/MM/yyyy");
+        txtTime.append(dinhDangNgay.format(calendar.getTime()));
+
+        txtTime.append(calendar.get(Calendar.HOUR)+"\n");// định dạng 12h
+        txtTime.append(calendar.get(Calendar.HOUR_OF_DAY)+"\n");// định dạng 24h
+
+        SimpleDateFormat dinhDangGio = new SimpleDateFormat("hh:mm:ss ");
+        txtTime.append(dinhDangGio.format(calendar.getTime()));
+
+
     }
 }
