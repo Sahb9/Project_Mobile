2cbe69652a0eebd255f1850d5ef0e95441be44b1
ChiVi123 <nguyenhoangchivi@gmail.com>
2022-05-14 22:53:01 +0700

    add theme for item habit

--- app/src/main/java/com/android/myapplication/entitys/HabitAdapter.java
+++ app/src/main/java/com/android/myapplication/entitys/HabitAdapter.java
@@ -1,10 +1,12 @@
 package com.android.myapplication.entitys;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -14,12 +16,17 @@ import com.android.myapplication.R;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class HabitAdapter extends BaseAdapter {
     private Context context;
     private int layout;
     private List<Habit> habitList;
+    private int[] themes = {R.drawable.gradient_button1, R.drawable.gradient_button2, R.drawable.gradient_button3};
+    private Random random = new Random();
+    private byte[] bytes = new byte[1];
 
     public HabitAdapter(Context context, int layout, List<Habit> habitList) {
         this.context = context;
@@ -46,18 +53,33 @@ public class HabitAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        LayoutInflater layoutInflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        view = layoutInflater.inflate(this.layout, null);
+        HabitAdapterHolder habitAdapterHolder;
 
-        TextView textViewName = (TextView) view.findViewById(R.id.habit_name);
-        TextView textViewProgress = view.findViewById(R.id.txt_habit_progress);
-        ProgressBar progressBarHabit = view.findViewById(R.id.progress_circular);
+        if (view == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = layoutInflater.inflate(this.layout, null);
+
+            habitAdapterHolder = new HabitAdapterHolder();
+
+            habitAdapterHolder.linearLayout = view.findViewById(R.id.habit_item);
+            habitAdapterHolder.textViewName = view.findViewById(R.id.habit_name);
+            habitAdapterHolder.textViewProgress = view.findViewById(R.id.txt_habit_progress);
+            habitAdapterHolder.progressBarHabit = view.findViewById(R.id.progress_circular);
+            habitAdapterHolder.position = this.random.nextInt(3);
+            this.random.nextBytes(bytes);
+            habitAdapterHolder.progress = (byte) (Math.abs(this.bytes[0]) % 100);
+
+            view.setTag(habitAdapterHolder);
+        } else {
+            habitAdapterHolder = (HabitAdapterHolder) view.getTag();
+        }
 
         Habit habit = this.habitList.get(i);
 
-        textViewName.setText(habit.getName());
-        textViewProgress.setText("50%");
-        progressBarHabit.setProgress(50);
+        habitAdapterHolder.linearLayout.setBackgroundResource(this.themes[habitAdapterHolder.position]);
+        habitAdapterHolder.textViewName.setText(habit.getName());
+        habitAdapterHolder.textViewProgress.setText(habitAdapterHolder.progress + "%");
+        habitAdapterHolder.progressBarHabit.setProgress(habitAdapterHolder.progress);
 
         return view;
     }
---
+++ app/src/main/java/com/android/myapplication/entitys/HabitAdapterHolder.java
@@ -0,0 +1,17 @@
+package com.android.myapplication.entitys;
+
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class HabitAdapterHolder {
+    public LinearLayout linearLayout;
+    public TextView textViewName;
+    public TextView textViewProgress;
+    public ProgressBar progressBarHabit;
+    public int position;
+    public byte progress;
+
+    public HabitAdapterHolder() {
+    }
+}
